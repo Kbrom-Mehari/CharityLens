60972637c1e4d3dffcbd42d8ae3de98a9113a0d5
Kbrom Mehari <kibrommehari121@gmail.com>
2025-07-16 15:57:32 +0300

    fundamental files for backend

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/CharityLensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kbrom.charity_lens_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityLensBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityLensBackendApplication.class, args);
+	}
+
+}
