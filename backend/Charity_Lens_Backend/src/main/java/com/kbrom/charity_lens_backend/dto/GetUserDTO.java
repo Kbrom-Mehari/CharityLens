b8b7e5738e941bf76678623d2686bd0669110815
Kbrom Mehari <kibrommehari121@gmail.com>
2025-07-21 11:32:26 +0300

    DTO for user entity to prevent sensitive data exposure

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/dto/GetUserDTO.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/dto/GetUserDTO.java
@@ -1,15 +1,4 @@
 package com.kbrom.charity_lens_backend.dto;
 
-import com.kbrom.charity_lens_backend.model.enums.Role;
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
-public class UserInfoDTO {
-    private Long id;
-    private String firstName;
-    private  String lastName;
-    private String email;
-    private Role role;
+public class GetUserDTO {
 }
