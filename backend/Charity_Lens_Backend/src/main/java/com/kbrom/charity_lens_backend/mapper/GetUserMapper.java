4d1c20a35151895e513b286820b2cb4f88c475f4
Kbrom Mehari <kibrommehari121@gmail.com>
2025-07-21 11:38:31 +0300

    mapper interface that maps DTOs to real entities

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/mapper/GetUserMapper.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/mapper/GetUserMapper.java
@@ -1,4 +1,15 @@
 package com.kbrom.charity_lens_backend.mapper;
 
+import com.kbrom.charity_lens_backend.dto.GetUserDTO;
+import com.kbrom.charity_lens_backend.model.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface GetUserMapper {
+    GetUserDTO toDTO(User user);
+    User toUser(GetUserDTO getUserDTO);
+    List<GetUserDTO> toListDTO(List<User> users);
+    List<User> toUsers(List<GetUserDTO> getUserDTOs);
 }
--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/mapper/RegisterUserMapper.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/mapper/RegisterUserMapper.java
@@ -1,4 +1,9 @@
 package com.kbrom.charity_lens_backend.mapper;
 
+import com.kbrom.charity_lens_backend.dto.RegisterUserDTO;
+import com.kbrom.charity_lens_backend.model.User;
+
 public interface RegisterUserMapper {
+    RegisterUserDTO toDTO(User user);
+    User toUser(RegisterUserDTO registerUserDTO);
 }
