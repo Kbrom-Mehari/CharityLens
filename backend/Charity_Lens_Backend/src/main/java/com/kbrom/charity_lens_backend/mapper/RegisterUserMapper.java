336d90de998c43302f1fc4de6ad2980acc7582cc
Kbrom Mehari <kibrommehari121@gmail.com>
2025-07-22 21:23:26 +0300

    added @Mapper annotation

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/mapper/RegisterUserMapper.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/mapper/RegisterUserMapper.java
@@ -2,7 +2,9 @@ package com.kbrom.charity_lens_backend.mapper;
 
 import com.kbrom.charity_lens_backend.dto.RegisterUserDTO;
 import com.kbrom.charity_lens_backend.model.User;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface RegisterUserMapper {
     RegisterUserDTO toDTO(User user);
     User toUser(RegisterUserDTO registerUserDTO);
