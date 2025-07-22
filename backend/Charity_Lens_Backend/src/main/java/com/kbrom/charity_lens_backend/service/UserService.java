e0bd9a8d22d507985421567be5782dd4a1bdb691
Kbrom Mehari <kibrommehari121@gmail.com>
2025-07-22 21:30:47 +0300

    I updated the update logic to allow updating only specific fields

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/service/UserService.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/service/UserService.java
@@ -1,7 +1,11 @@
 package com.kbrom.charity_lens_backend.service;
 
 import com.kbrom.charity_lens_backend.dto.GetUserDTO;
+import com.kbrom.charity_lens_backend.dto.RegisterUserDTO;
+import com.kbrom.charity_lens_backend.dto.UpdateUserDTO;
+import com.kbrom.charity_lens_backend.exception.ResourceNotFoundException;
 import com.kbrom.charity_lens_backend.mapper.GetUserMapper;
+import com.kbrom.charity_lens_backend.mapper.RegisterUserMapper;
 import com.kbrom.charity_lens_backend.model.User;
 import com.kbrom.charity_lens_backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +18,12 @@ import java.util.List;
 public class UserService {
       private final UserRepository userRepository;
       private final GetUserMapper getUserMapper;
+      private final RegisterUserMapper registerUserMapper;
       public GetUserDTO getUserByEmail(String email) {
         User user= userRepository.findByEmail(email);
+        if(user==null) {
+            throw new ResourceNotFoundException("User Not Found");
+        }
         return getUserMapper.toDTO(user);
       }
       public List<GetUserDTO> getAllUsers(){
@@ -23,8 +31,33 @@ public class UserService {
           return getUserMapper.toListDTO(users);
       }
       public GetUserDTO getUserById(Long id) {
-          User user = userRepository.findById(id).get();
+          User user = userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User Not Found"));
           return getUserMapper.toDTO(user);
+      }
+      public void deleteUserById(Long id) {
+          User user = userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User Not Found"));
+          userRepository.delete(user);
+      }
+      public GetUserDTO createUser(RegisterUserDTO registerUserDTO){
+          User user = registerUserMapper.toUser(registerUserDTO);
+          User createdUser= userRepository.save(user);
+          return getUserMapper.toDTO(createdUser);
+      }
+      public GetUserDTO updateUser(UpdateUserDTO updateUserDTO, Long id) {
+          User oldUser=userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User Not Found"));
+          if(updateUserDTO.getFirstName()!=null) {
+              oldUser.setFirstName(updateUserDTO.getFirstName());
+          }
+          if(updateUserDTO.getLastName()!=null) {
+              oldUser.setLastName(updateUserDTO.getLastName());
+          }
+          if(updateUserDTO.getUsername()!=null) {
+              oldUser.setUsername(updateUserDTO.getUsername());
+          }
+          if(updateUserDTO.getPassword()!=null) {
+              oldUser.setPassword(updateUserDTO.getPassword());
+          }
+          return getUserMapper.toDTO(oldUser);
       }
 
 }
