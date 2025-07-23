0f84142b22369dd1fa1e71b797e71628449ae183
Kbrom Mehari <kibrommehari121@gmail.com>
2025-07-23 21:50:51 +0300

    donation entity

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/model/Donation.java
@@ -0,0 +1,25 @@
+package com.kbrom.charity_lens_backend.model;
+
+import com.kbrom.charity_lens_backend.model.enums.Currency;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Donation {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne    //Many donations can be made to a project
+    private Project project;
+    @ManyToOne   //Many donations can be made for a campaign
+    private Campaign campaign;
+    @ManyToOne    // A user can make many donations
+    private User donor;
+    @Column(precision=10, scale = 2,nullable = false)
+    private BigDecimal donationAmount;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Currency currency;
+
+}
