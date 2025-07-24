6bc32c6c7c5d3cd77bd35d36cefeb50f274cbb0a
Kbrom Mehari <kibrommehari121@gmail.com>
2025-07-25 00:08:57 +0300

    unauthorized exception

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/exception/UnauthorizedException.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/exception/UnauthorizedException.java
@@ -1,5 +1,6 @@
 package com.kbrom.charity_lens_backend.exception;
 
+// for unauthorized activities
 public class UnauthorizedException extends RuntimeException {
     public UnauthorizedException(String message) {
         super(message);
