07231d828cbf141dbf18bb6c5bea8670ccb15113
Kbrom Mehari <kibrommehari121@gmail.com>
2025-07-27 22:06:43 +0300

    this'll be used to seed focus areas to the database at runtime

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/startup/FocusAreaSeeder.java
@@ -0,0 +1,12 @@
+package com.kbrom.charity_lens_backend.startup;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FocusAreaSeeder implements CommandLineRunner {
+    @Override
+    public void run (String... args){
+
+    }
+}
