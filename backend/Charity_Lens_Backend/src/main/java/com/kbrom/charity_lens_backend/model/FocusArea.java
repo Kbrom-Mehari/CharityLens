770600d891e055c4ea52051b7d489ca7aa56787d
Kbrom Mehari <kibrommehari121@gmail.com>
2025-07-27 23:11:24 +0300

    focus areas for organizations

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/model/FocusArea.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/model/FocusArea.java
@@ -1,5 +1,17 @@
 package com.kbrom.charity_lens_backend.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class FocusArea {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String sdgGoal;
 
 }
