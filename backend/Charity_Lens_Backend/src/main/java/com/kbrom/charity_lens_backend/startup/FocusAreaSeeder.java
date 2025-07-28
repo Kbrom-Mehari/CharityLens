8c90d3cdf98d7918e29f285e220c551a0c1201f3
Kbrom Mehari <kibrommehari121@gmail.com>
2025-07-28 23:27:30 +0300

    this is used to populate focus areas to the database when the app runs

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/startup/FocusAreaSeeder.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/startup/FocusAreaSeeder.java
@@ -1,12 +1,27 @@
 package com.kbrom.charity_lens_backend.startup;
 
+import com.kbrom.charity_lens_backend.model.FocusArea;
+import com.kbrom.charity_lens_backend.repository.FocusAreaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Component
-public class FocusAreaSeeder implements CommandLineRunner {
-    @Override
-    public void run (String... args){
 
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class FocusAreaSeeder {
+    private final FocusAreaRepository focusAreaRepository;
+
+    @Bean
+    CommandLineRunner seedFocusAreas() {
+        return args -> focusAreaRepository.saveAll(List.of(new  FocusArea("","",""),
+                                                                    new FocusArea("","",""),
+                                                                    new FocusArea("","",""),
+                                                                    new FocusArea("","","")));
     }
+
+
 }
