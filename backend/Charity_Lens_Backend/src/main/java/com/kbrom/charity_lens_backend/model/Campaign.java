4ad5f67ded000be2989a8d0ca4302d5132eb9bc1
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-06 14:11:27 +0300

    initialized the totalDonations field to prevent null pointer exception

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/model/Campaign.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/model/Campaign.java
@@ -1,12 +1,16 @@
 package com.kbrom.charity_lens_backend.model;
 
+import com.kbrom.charity_lens_backend.model.enums.Cause;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
+@Getter
+@Setter
 @Entity
 public class Campaign {
     @Id
@@ -17,8 +21,11 @@ public class Campaign {
     private String objective;
     private String description;
     private BigDecimal donationGoal;
-    private LocalDate startDate;
+    private LocalDate startDate=LocalDate.now();
     private LocalDate endDate;
     @OneToMany(mappedBy="campaign")
     private List<Donation> donations=new ArrayList<>();
+    private BigDecimal totalDonation=BigDecimal.ZERO;
+    @Enumerated(EnumType.STRING)
+    private Cause cause;
 }
