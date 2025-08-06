1775e1f993b7c3b7d9fe4c269f7005feb8a47fb6
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-06 14:18:59 +0300

    added custom JPA query method to find projects achieved their donation goal

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/repository/ProjectRepository.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/repository/ProjectRepository.java
@@ -1,4 +1,16 @@
 package com.kbrom.charity_lens_backend.repository;
 
-public interface ProjectRepository {
+import com.kbrom.charity_lens_backend.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findAllByActive(boolean active);
+    List<Project> findAllByOrganizationId(Long id);
+    @Query("select p from Project p where p.totalDonation>=p.donationGoal")
+    List<Project> findAllByDonationGoalAchieved();
 }
