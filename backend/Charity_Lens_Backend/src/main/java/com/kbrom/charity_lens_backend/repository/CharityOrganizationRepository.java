c8d038c8df16ff456ee808b9a43940d697e325a5
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-06 14:16:25 +0300

    added a method for searching organizations

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/repository/CharityOrganizationRepository.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/repository/CharityOrganizationRepository.java
@@ -3,6 +3,12 @@ package com.kbrom.charity_lens_backend.repository;
 import com.kbrom.charity_lens_backend.model.CharityOrganization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CharityOrganizationRepository extends JpaRepository<CharityOrganization, Long> {
+    CharityOrganization findByEmail(String email);
+    CharityOrganization findById(long id);
+    List<CharityOrganization> searchCharityOrganizationByNameContainingIgnoreCase(String name);
+
 
 }
