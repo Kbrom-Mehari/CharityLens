3dd09cac028bd270b816573baff75a7ba86ea9af
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-07 23:36:43 +0300

    fixed url names

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/controller/ProjectController.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/controller/ProjectController.java
@@ -1,9 +1,10 @@
 package com.kbrom.charity_lens_backend.controller;
 
+import com.kbrom.charity_lens_backend.dto.CreateProjectDTO;
 import com.kbrom.charity_lens_backend.dto.GetProjectDTO;
-import com.kbrom.charity_lens_backend.model.Project;
 import com.kbrom.charity_lens_backend.service.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,16 +14,30 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProjectController {
     private final ProjectService projectService;
+    @PostMapping
+    public ResponseEntity<GetProjectDTO> createProject(@RequestBody CreateProjectDTO createProjectDTO){
+        GetProjectDTO project =projectService.createProject(createProjectDTO);
+        return ResponseEntity.ok(project);
+    }
     @GetMapping
-    public List<GetProjectDTO> getAllProjects() {
-        return projectService.findAllProjects();
+    public ResponseEntity<List<GetProjectDTO>> getAllProjects() {  // needs pagination + filtering later
+        List<GetProjectDTO> projects= projectService.findAllProjects();
+        return ResponseEntity.ok(projects);
     }
-    @GetMapping("{id}")
-    public GetProjectDTO getProjectById(@PathVariable Long id) {
-        return projectService.findProjectById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<GetProjectDTO> getProjectById(@PathVariable Long id) {
+        GetProjectDTO project= projectService.findProjectById(id);
+        return ResponseEntity.ok(project);
     }
-    @GetMapping()
-    public List<GetProjectDTO> findProjectsByOrgId(@RequestParam Long organizationId) {
-        return projectService.findProjectsByOrganizationId(organizationId);
+    @GetMapping("/by-organization/{organizationId}")  // needs pagination + filtering later
+    public ResponseEntity<List<GetProjectDTO>> findProjectsByOrgId(@PathVariable Long organizationId) {
+        List<GetProjectDTO> projects= projectService.findProjectsByOrganizationId(organizationId);
+        return ResponseEntity.ok(projects);
     }
+    @PatchMapping("/{id}/terminate")
+    public ResponseEntity<String> terminateProject(@PathVariable Long id) {
+       return ResponseEntity.ok().body(projectService.terminateProjectById(id));
+    }
+
+
 }
