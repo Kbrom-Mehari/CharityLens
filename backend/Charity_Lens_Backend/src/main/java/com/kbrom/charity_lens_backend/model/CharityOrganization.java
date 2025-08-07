47f3bae3044ae7e7876635e6050e8c8552e5118e
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-07 23:42:07 +0300

    added join table with focusArea

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/model/CharityOrganization.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/model/CharityOrganization.java
@@ -13,15 +13,24 @@ public class CharityOrganization {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
     private  String name;
+    @Column(nullable = false)
+    private  String phone;
     @Column(nullable = false,unique = true)
     private  String email;
     @Column(nullable = false)
-    private  String phone;
-    @OneToMany
+    private String password;
+    @OneToMany(mappedBy="organization")
     private List<Project> projects=new ArrayList<>();
     private boolean isFlagged=false;
+    @ManyToMany
+    @JoinTable(
+        name="charity_organization_focus_areas",
+        joinColumns=@JoinColumn(name="organization_id"),
+        inverseJoinColumns=@JoinColumn(name="focus_area_id")
+    )
+    private List<FocusArea> focusAreas=new ArrayList<>();
 
 
 }
