355bb6456f1877d3521992cf759325fde47b3006
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-09 22:45:23 +0300

    basic crud methods for projects

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/service/ProjectService.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/service/ProjectService.java
@@ -1,7 +1,9 @@
 package com.kbrom.charity_lens_backend.service;
 
-import com.kbrom.charity_lens_backend.dto.ProjectDTO;
+import com.kbrom.charity_lens_backend.dto.CreateProjectDTO;
+import com.kbrom.charity_lens_backend.dto.GetProjectDTO;
 import com.kbrom.charity_lens_backend.exception.ResourceNotFoundException;
+import com.kbrom.charity_lens_backend.mapper.GetProjectMapper;
 import com.kbrom.charity_lens_backend.model.CharityOrganization;
 import com.kbrom.charity_lens_backend.model.Project;
 import com.kbrom.charity_lens_backend.repository.CharityOrganizationRepository;
@@ -17,8 +19,9 @@ import java.util.List;
 public class ProjectService {
     private final ProjectRepository projectRepository;
     private final CharityOrganizationRepository charityOrganizationRepository;
+    private final GetProjectMapper getProjectMapper;
 
-    public Project createProject(ProjectDTO projectDTO) {
+    public GetProjectDTO createProject(CreateProjectDTO projectDTO) {
         CharityOrganization org=charityOrganizationRepository.findById(projectDTO.getOrganizationId()).orElseThrow(()-> new ResourceNotFoundException("Organization not found"));
         Project project = new Project();
         project.setTitle(projectDTO.getTitle());
@@ -26,29 +29,37 @@ public class ProjectService {
         project.setDonationGoal(projectDTO.getDonationGoal());
         project.setCause(projectDTO.getCause());
         project.setOrganization(org);
-        return projectRepository.save(project);
+        projectRepository.save(project);
+        return getProjectMapper.toDTO(project);
     }
-    public List<Project> findAllProjects() {
-        return projectRepository.findAll();
+    public List<GetProjectDTO> findAllProjects() {
+        List<Project> projects= projectRepository.findAll();
+        return getProjectMapper.toDTO_List(projects);
     }
-    public List<Project> findActiveProjects() {
-        return projectRepository.findAllByActive(true);
+    public List<GetProjectDTO> findActiveProjects() {      // needs pagination later here
+        List<Project> projects= projectRepository.findAllByActive(true);
+        return getProjectMapper.toDTO_List(projects);
     }
-    public Project findProjectById(Long id) {
-        return projectRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Project not found"));
+    public GetProjectDTO findProjectById(Long id) {
+        Project project=projectRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Project not found"));
+        return getProjectMapper.toDTO(project);
     }
-    public List<Project> findProjectsByOrganizationId(Long id) {
-        return projectRepository.findAllByOrganizationId(id);
+    public List<GetProjectDTO> findProjectsByOrganizationId(Long id) {
+        List<Project> projects= projectRepository.findAllByOrganizationId(id);
+        return getProjectMapper.toDTO_List(projects);
     }
-    @Transactional      // to
+
+    @Transactional      // to persist the change automatically when method is called
     public String terminateProjectById(Long id) {
-        Project project = findProjectById(id);
+        Project project = projectRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Project not found"));
         if (!project.isActive()) {
             return "Project is already terminated";
         }
         project.setActive(false);
         return "Project terminated";
     }
-
-
+    public List<GetProjectDTO> getFullyFundedProjects() {    //needs pagination here
+        List<Project> projects=projectRepository.findAllByDonationGoalAchieved();
+        return getProjectMapper.toDTO_List(projects);
+    }
 }
