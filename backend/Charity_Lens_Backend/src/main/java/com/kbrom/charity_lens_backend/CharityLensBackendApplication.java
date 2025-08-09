db15402a45c108820a6c9a41cd1fdc8106bed829
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-09 22:40:09 +0300

    added @EnableConfigurationProperties for JwtProperties class

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/CharityLensBackendApplication.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/CharityLensBackendApplication.java
@@ -1,9 +1,12 @@
 package com.kbrom.charity_lens_backend;
 
+import com.kbrom.charity_lens_backend.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class CharityLensBackendApplication {
 
 	public static void main(String[] args) {
