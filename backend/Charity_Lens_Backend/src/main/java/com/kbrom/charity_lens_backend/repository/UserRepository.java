6326e72d7437dc11cc2b7d42e6f2f949d6994e83
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-10 22:32:39 +0300

    changed the return type of the fields Optional<User>

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/repository/UserRepository.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.kbrom.charity_lens_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-    User findByUsername(String username);
-    User findByEmailAndPassword(String email, String password);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmailAndPassword(String email, String password);
 }
