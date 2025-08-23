74d8478e51df9bbe6e2b7361240b6eb4e9f890c5
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-23 22:55:05 +0300

    changed to feature based structure

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/campaign/dto/CreateCampaignDTO.java
@@ -0,0 +1,19 @@
+package com.kbrom.charity_lens_backend.campaign.dto;
+
+import com.kbrom.charity_lens_backend.common.enums.Cause;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class CreateCampaignDTO {
+    private String title;
+    private String objective;
+    private String description;
+    private BigDecimal donationGoal;
+    private LocalDate endDate;
+    private Cause cause;
+}
