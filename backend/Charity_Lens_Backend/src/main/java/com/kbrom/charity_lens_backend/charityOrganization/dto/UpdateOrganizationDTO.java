318813df5dd9ff108219a0bafd98a0326d19dfac
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-24 23:35:24 +0300

    for updating an org

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/charityOrganization/dto/UpdateOrganizationDTO.java
@@ -0,0 +1,10 @@
+package com.kbrom.charity_lens_backend.charityOrganization.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateOrganizationDTO {
+    private String name;
+    private String phone;
+    private String password;
+}
