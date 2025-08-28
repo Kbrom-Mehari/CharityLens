e653edd7f1b6412c9932c5f4829408e3852af0e4
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-28 22:47:27 +0300

    moved

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.kbrom.charity_lens_backend.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
