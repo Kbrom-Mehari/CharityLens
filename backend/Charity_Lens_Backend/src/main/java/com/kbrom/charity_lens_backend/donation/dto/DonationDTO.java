60afb2fa021e84206631e3dc2dc061f8deb2c142
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-29 21:04:16 +0300

    donation DTO

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/donation/dto/DonationDTO.java
@@ -0,0 +1,21 @@
+package com.kbrom.charity_lens_backend.donation.dto;
+
+import com.kbrom.charity_lens_backend.common.enums.Currency;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class DonationDTO {
+    private Long donationId;
+    private String donorFirstName;
+    private String donorLastName;
+    private Long projectId;
+    private Long campaignId;
+    private BigDecimal amount;
+    private Currency currency;
+    private LocalDateTime createdAt;
+}
