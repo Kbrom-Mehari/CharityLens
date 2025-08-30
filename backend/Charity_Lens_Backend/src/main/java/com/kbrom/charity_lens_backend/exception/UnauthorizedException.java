0ffea0dad2b5f1fab74390877bb0d31e696fe656
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-30 13:48:06 +0300

    moved

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.kbrom.charity_lens_backend.exception;
-
-// for unauthorized activities
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
