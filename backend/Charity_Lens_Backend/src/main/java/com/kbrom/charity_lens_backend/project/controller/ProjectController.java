fed35e7139df19182f56cdad5b153ca43826d1bd
Kbrom Mehari <kibrommehari121@gmail.com>
2025-08-31 22:06:38 +0300

    project controller

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/project/controller/ProjectController.java
@@ -0,0 +1,43 @@
+package com.kbrom.charity_lens_backend.project.controller;
+
+import com.kbrom.charity_lens_backend.project.dto.CreateProjectDTO;
+import com.kbrom.charity_lens_backend.project.dto.GetProjectDTO;
+import com.kbrom.charity_lens_backend.project.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+    private final ProjectService projectService;
+    @PostMapping
+    public ResponseEntity<GetProjectDTO> createProject(@RequestBody CreateProjectDTO createProjectDTO){
+        GetProjectDTO project =projectService.createProject(createProjectDTO);
+        return ResponseEntity.ok(project);
+    }
+    @GetMapping
+    public ResponseEntity<List<GetProjectDTO>> getAllProjects() {  // needs pagination + filtering later
+        List<GetProjectDTO> projects= projectService.findAllProjects();
+        return ResponseEntity.ok(projects);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<GetProjectDTO> getProjectById(@PathVariable Long id) {
+        GetProjectDTO project= projectService.findProjectById(id);
+        return ResponseEntity.ok(project);
+    }
+    @GetMapping("/by-organization/{organizationId}")  // needs pagination + filtering later
+    public ResponseEntity<List<GetProjectDTO>> findProjectsByOrgId(@PathVariable Long organizationId) {
+        List<GetProjectDTO> projects= projectService.findProjectsByOrganizationId(organizationId);
+        return ResponseEntity.ok(projects);
+    }
+    @PatchMapping("/{id}/terminate")
+    public ResponseEntity<String> terminateProject(@PathVariable Long id) {
+       return ResponseEntity.ok().body(projectService.terminateProjectById(id));
+    }
+
+
+}
