30b50f64551c6fc20c8d8ebdc29d92dc554cd58a
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-02 13:38:59 +0300

    moved

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/repository/FocusAreaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kbrom.charity_lens_backend.repository;
-
-import com.kbrom.charity_lens_backend.model.FocusArea;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FocusAreaRepository extends JpaRepository<FocusArea, Long> {
-
-}
