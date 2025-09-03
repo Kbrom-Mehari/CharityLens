48876faf3ab5bba207f18538fbaad153bd29f0c0
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-04 00:09:15 +0300

    DTO to register charity org

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/auth/dto/RegisterCharityOrganizationDTO.java
@@ -0,0 +1,27 @@
+package com.kbrom.charity_lens_backend.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterCharityOrganizationDTO {
+    private String organizationName;
+
+    private String phoneNumber;
+    @Pattern(regexp="^(?!.*[._])[a-zA-Z0-9](?:[a-zA-Z0-9._]{1,18}[a-zA-Z0-9])?$"
+            ,message = "username cannot have special characters, must be 3-20 characters,start/end with letter/number,cannot have consecutive . or _")
+    @NotBlank(message = "username is required")
+    private String username;
+    @Email(message = "enter a valid email address")
+    @NotBlank(message = "email is required")
+    private String email;
+    @NotBlank(message = "password is required")
+    @Size(min=8,max=20,message = "password must be between 8-20")
+    private String password;
+
+}
