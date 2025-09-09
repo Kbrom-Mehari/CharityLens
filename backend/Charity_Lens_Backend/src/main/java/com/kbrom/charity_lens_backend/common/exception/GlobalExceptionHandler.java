df268f5458a54fc1067f6c12023a05c7fe2421fa
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-09 22:38:38 +0300

    handling more exceptions

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/common/exception/GlobalExceptionHandler.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.kbrom.charity_lens_backend.common.exception;
 
+import com.kbrom.charity_lens_backend.auth.exception.InvalidCredentialsException;
+import com.kbrom.charity_lens_backend.auth.exception.TokenGenerationException;
+import com.kbrom.charity_lens_backend.auth.exception.TokenValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +17,10 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e) {
     return buildErrorResponse(HttpStatus.NOT_FOUND,e.getMessage());
 }
+@ExceptionHandler(TokenGenerationException.class)
+public ResponseEntity<ErrorResponse> handleTokenGenerationException(TokenGenerationException e) {
+    return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR,e.getMessage());
+}
 @ExceptionHandler(DuplicateEntryException.class)
 public ResponseEntity<ErrorResponse> handleDuplicateEntryException(DuplicateEntryException e) {
     return buildErrorResponse(HttpStatus.CONFLICT,e.getMessage());
@@ -21,6 +28,22 @@ public ResponseEntity<ErrorResponse> handleDuplicateEntryException(DuplicateEntr
 @ExceptionHandler(MethodArgumentNotValidException.class)
 public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
     return buildErrorResponse(HttpStatus.BAD_REQUEST,e.getMessage());
+}
+@ExceptionHandler(InvalidCredentialsException.class)
+public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException e){
+    return buildErrorResponse(HttpStatus.UNAUTHORIZED,e.getMessage());
+}
+@ExceptionHandler(TokenValidationException.class)
+public ResponseEntity<ErrorResponse> handleTokenValidationException(TokenValidationException e) {
+    return buildErrorResponse(HttpStatus.UNAUTHORIZED,e.getMessage());
+}
+@ExceptionHandler(BadRequestException.class)
+public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
+    return buildErrorResponse(HttpStatus.BAD_REQUEST,e.getMessage());
+}
+@ExceptionHandler(UnauthorizedException.class)
+public ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException e) {
+    return buildErrorResponse(HttpStatus.UNAUTHORIZED,e.getMessage());
 }
 private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status,String message){
     ErrorResponse response=new ErrorResponse(status.value(),message,status.getReasonPhrase(), LocalDateTime.now());
