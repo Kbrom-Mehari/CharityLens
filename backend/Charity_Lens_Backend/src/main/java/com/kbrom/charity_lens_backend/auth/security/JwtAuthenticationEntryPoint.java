1d1a81350fa170268cd64301025c62f3f9854aff
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-11 22:01:44 +0300

    build the error message using map

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/auth/security/JwtAuthenticationEntryPoint.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/auth/security/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,35 @@
 package com.kbrom.charity_lens_backend.auth.security;
 
-public class JwtAuthenticationEntryPoint {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    ObjectMapper objectMapper = new ObjectMapper();
+    public void commence (HttpServletRequest request,
+                          HttpServletResponse response,
+                          AuthenticationException authenticationException)throws IOException {
+
+        Map<String,Object> errorDetails = new HashMap<>();
+        errorDetails.put("timestamp", LocalDateTime.now().toString());
+        errorDetails.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        errorDetails.put("message", authenticationException.getMessage());
+        errorDetails.put("error","Unauthorized");
+        errorDetails.put("path",request.getRequestURI());
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+
+        response.getWriter().write(objectMapper.writeValueAsString(errorDetails));
+
+
+
+    }
 }
