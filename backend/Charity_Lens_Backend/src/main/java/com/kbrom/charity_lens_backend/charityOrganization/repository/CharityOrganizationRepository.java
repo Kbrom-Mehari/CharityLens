146b97fc92740767cc767a245eeffce4fc4d91c6
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-11 21:55:20 +0300

    removed the findByEmail method

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/charityOrganization/repository/CharityOrganizationRepository.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/charityOrganization/repository/CharityOrganizationRepository.java
@@ -4,11 +4,9 @@ import com.kbrom.charity_lens_backend.charityOrganization.model.CharityOrganizat
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CharityOrganizationRepository extends JpaRepository<CharityOrganization, Long> {
-    Optional<CharityOrganization> findByEmail(String email);
-    List<CharityOrganization> searchCharityOrganizationByNameContainingIgnoreCase(String name);
+    List<CharityOrganization> searchCharityOrganizationByOrganizationNameContainingIgnoreCase(String name);
 
 
 }
