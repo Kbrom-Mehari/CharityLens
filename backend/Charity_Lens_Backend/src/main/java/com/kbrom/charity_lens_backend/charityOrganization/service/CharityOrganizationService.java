93e1f06e0ac08065e2adeee586a81e63b9b94c98
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-11 21:56:21 +0300

    removed the register user method

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/charityOrganization/service/CharityOrganizationService.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/charityOrganization/service/CharityOrganizationService.java
@@ -1,11 +1,9 @@
 package com.kbrom.charity_lens_backend.charityOrganization.service;
 
 import com.kbrom.charity_lens_backend.charityOrganization.dto.GetOrganizationDTO;
-import com.kbrom.charity_lens_backend.charityOrganization.dto.RegisterOrganizationDTO;
 import com.kbrom.charity_lens_backend.charityOrganization.dto.UpdateOrganizationDTO;
 import com.kbrom.charity_lens_backend.common.exception.ResourceNotFoundException;
 import com.kbrom.charity_lens_backend.common.util.mappers.GetOrganizationMapper;
-import com.kbrom.charity_lens_backend.common.util.mappers.RegisterOrganizationMapper;
 import com.kbrom.charity_lens_backend.charityOrganization.model.CharityOrganization;
 import com.kbrom.charity_lens_backend.charityOrganization.repository.CharityOrganizationRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +16,8 @@ import java.util.List;
 public class CharityOrganizationService {
     private final GetOrganizationMapper getOrganizationMapper;
     private final CharityOrganizationRepository charityOrganizationRepository;
-    private final RegisterOrganizationMapper registerOrganizationMapper;
 
-    public GetOrganizationDTO registerOrganization(RegisterOrganizationDTO registerOrganizationDTO) {
-        CharityOrganization organization=registerOrganizationMapper.toOrganization(registerOrganizationDTO);
-        charityOrganizationRepository.save(organization);
-        return getOrganizationMapper.toDTO(organization);
-    }
+
     public List<GetOrganizationDTO> findAllOrganizations() {
         List<CharityOrganization> organizations= charityOrganizationRepository.findAll();
         return getOrganizationMapper.toDTO_List(organizations);
@@ -34,14 +27,8 @@ public class CharityOrganizationService {
                          .orElseThrow(()->new ResourceNotFoundException("Organization not found"));
         return getOrganizationMapper.toDTO(organization);
     }
-    public GetOrganizationDTO getOrganizationByEmail(String email) {
-        CharityOrganization organization = charityOrganizationRepository.findByEmail(email).orElseThrow(()->new ResourceNotFoundException("Organization not found"));
-        return getOrganizationMapper.toDTO(organization);
-    }
-    public List<GetOrganizationDTO> searchOrganizationsByName(String name){
-        List<CharityOrganization> organizations= charityOrganizationRepository.searchCharityOrganizationByNameContainingIgnoreCase(name);
-        return getOrganizationMapper.toDTO_List(organizations);
-    }
+
+
     public void deleteById(Long id) {
         CharityOrganization organization=charityOrganizationRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Organization not found"));
         charityOrganizationRepository.delete(organization);
@@ -49,14 +36,9 @@ public class CharityOrganizationService {
     public GetOrganizationDTO updateOrganization(Long id ,UpdateOrganizationDTO updateOrganizationDTO) {
         CharityOrganization updated=charityOrganizationRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Organization not found"));
         if(updateOrganizationDTO.getName()!=null){
-            updated.setName(updateOrganizationDTO.getName());
+            updated.setOrganizationName(updateOrganizationDTO.getName());
         }
-        if(updateOrganizationDTO.getPhone()!=null){
-            updated.setPhone(updateOrganizationDTO.getPhone());
-        }
-        if(updateOrganizationDTO.getPassword()!=null){
-            updated.setPassword(updateOrganizationDTO.getPassword());
-        }
+
         return getOrganizationMapper.toDTO(updated);
     }
 
