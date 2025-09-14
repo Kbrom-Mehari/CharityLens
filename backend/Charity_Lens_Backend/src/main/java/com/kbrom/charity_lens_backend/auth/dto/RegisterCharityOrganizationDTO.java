c709a5843ce438ea28cf93f39c4e3f3a25476823
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-14 23:11:27 +0300

    added @NotBlank to the to the organizationName field

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/auth/dto/RegisterCharityOrganizationDTO.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/auth/dto/RegisterCharityOrganizationDTO.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 public class RegisterCharityOrganizationDTO {
+    @NotBlank(message = "organization name is required")
     private String organizationName;
 
     private String phoneNumber;
