0ec681a9c451d0432a0793aa98e5e9cd4e7062b3
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-19 22:58:40 +0300

    change password DTO

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/auth/dto/ChangePasswordDTO.java
@@ -0,0 +1,11 @@
+package com.kbrom.charity_lens_backend.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChangePasswordDTO {
+    private String oldPassword;
+    private String newPassword;
+}
