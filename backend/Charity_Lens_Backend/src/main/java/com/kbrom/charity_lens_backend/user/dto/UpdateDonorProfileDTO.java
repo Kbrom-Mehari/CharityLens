4bab4e4e58f44c950528ee46be74593893c45aca
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-19 23:10:33 +0300

    DTO for updating donor profile

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/user/dto/UpdateDonorProfileDTO.java
@@ -0,0 +1,13 @@
+package com.kbrom.charity_lens_backend.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateDonorProfileDTO {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+}
