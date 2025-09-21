0b5b16a77b7347ce41b3bb5d0492d60380c88733
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-21 23:17:33 +0300

    separated profile fields to profile entity

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/user/model/User.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/user/model/User.java
@@ -1,40 +1,42 @@
 package com.kbrom.charity_lens_backend.user.model;
 
 import com.kbrom.charity_lens_backend.charityOrganization.model.CharityOrganization;
-import com.kbrom.charity_lens_backend.user.enums.Gender;
+import com.kbrom.charity_lens_backend.donorProfile.DonorProfile;
 import com.kbrom.charity_lens_backend.user.enums.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Check;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Setter
 @Getter
 @Entity
+@Check(constraints = "(organization_profile_id IS NULL OR donor_profile_id IS NULL)") //enforce a user cannot both profiles
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     @Column(nullable=false,unique = true)
     private String username;
-
-    private String firstName;
-
-    private String lastName;
     @Column(nullable = false,unique = true)
     private String email;
-    @Column(unique = true)
-    private String phoneNumber;
+    @Column(nullable = false)
     private String password;
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private List<Role> roles=new ArrayList<>();
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name="user_roles",
+            joinColumns=@JoinColumn(name="user_id")
+    )
+    private Set<Role> roles=EnumSet.noneOf(Role.class);
     private boolean isFlagged=false;
     private boolean isEnabled=false;
     @OneToOne(mappedBy = "user")
+    @JoinColumn(name="organization_profile_id")
     private CharityOrganization organizationProfile;
+    @OneToOne(mappedBy = "user")
+    @JoinColumn(name="donor_profile_id")
+    private DonorProfile profile;
 }
