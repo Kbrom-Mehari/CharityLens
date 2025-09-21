a418cf44ee6bb5412ed16adf2090021189279559
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-21 09:22:03 +0300

    role based access

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/user/controller/UserController.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/user/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.kbrom.charity_lens_backend.user.controller;
 
-import com.kbrom.charity_lens_backend.user.dto.RegisterUserDTO;
-import com.kbrom.charity_lens_backend.user.dto.UserUpdateDTO;
+import com.kbrom.charity_lens_backend.user.dto.UpdateDonorProfileDTO;
+import com.kbrom.charity_lens_backend.user.model.User;
 import com.kbrom.charity_lens_backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import com.kbrom.charity_lens_backend.user.dto.GetUserDTO;
+import com.kbrom.charity_lens_backend.user.dto.GetDonorProfileDTO;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,32 +16,32 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    @PreAuthorize("hasRole('SYS_ADMIN')")
     @GetMapping
-    public ResponseEntity<GetUserDTO> getUserByEmail(@RequestParam String email) {
-        GetUserDTO user= userService.getUserByEmail(email);
+    public ResponseEntity<User> getUserByEmail(@RequestParam String email) {
+        User user= userService.getUserByEmail(email);
         return ResponseEntity.ok(user);
     }
     @GetMapping("/{id}")
-    public ResponseEntity<GetUserDTO> getUserById(@PathVariable Long id) {
-        GetUserDTO user= userService.getUserById(id);
+    @PreAuthorize("hasRole('SYS_ADMIN')")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user= userService.getUserById(id);
         return ResponseEntity.ok(user);
     }
     @GetMapping
-    public ResponseEntity<List<GetUserDTO>> getAllUsers() {
-        List<GetUserDTO> users= userService.getAllUsers();
+    @PreAuthorize("hasRole('SYS_ADMIN')")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users= userService.getAllUsers();
         if(users.isEmpty()){
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(users);
     }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
-        userService.deleteUserById(id);
-        return ResponseEntity.noContent().build();
-    }
+
     @PatchMapping("/{id}")
-    public ResponseEntity<GetUserDTO> updateUser(@PathVariable Long id, UserUpdateDTO updateUserDTO) {
-        GetUserDTO updated= userService.updateUser(updateUserDTO,id);
+    @PreAuthorize("hasRole('DONOR') and #id==authentication.principal.id")
+    public ResponseEntity<GetDonorProfileDTO> updateDonorProfile(@PathVariable Long id, UpdateDonorProfileDTO updateUserDTO) {
+        GetDonorProfileDTO updated= userService.updateDonorProfile(updateUserDTO,id);
         return ResponseEntity.ok(updated);
     }
 }
