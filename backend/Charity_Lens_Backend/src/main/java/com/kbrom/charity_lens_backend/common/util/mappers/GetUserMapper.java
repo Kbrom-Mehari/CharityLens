84f1823f4363f432e067c78a09ef942258df84d4
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-25 22:51:59 +0300

    changed mapping to GetDonorProfileDTO

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/common/util/mappers/GetUserMapper.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/common/util/mappers/GetUserMapper.java
@@ -1,13 +1,10 @@
 package com.kbrom.charity_lens_backend.common.util.mappers;
 
-import com.kbrom.charity_lens_backend.user.dto.GetUserDTO;
-import com.kbrom.charity_lens_backend.user.model.User;
+import com.kbrom.charity_lens_backend.donorProfile.DonorProfile;
+import com.kbrom.charity_lens_backend.user.dto.GetDonorProfileDTO;
 import org.mapstruct.Mapper;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface GetUserMapper {
-    GetUserDTO toDTO(User user);
-    List<GetUserDTO> toDTO_List(List<User> users);
+    GetDonorProfileDTO toDTO(DonorProfile donorProfile);
 }
