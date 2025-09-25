d21123220eb46acd75a0f2e6b11e0179ffb28e02
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-25 23:02:21 +0300

    added change password and flag user methods

--- backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/user/service/UserService.java
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/user/service/UserService.java
@@ -1,56 +1,109 @@
 package com.kbrom.charity_lens_backend.user.service;
 
-import com.kbrom.charity_lens_backend.user.dto.GetUserDTO;
-import com.kbrom.charity_lens_backend.user.dto.RegisterUserDTO;
-import com.kbrom.charity_lens_backend.user.dto.UserUpdateDTO;
+import com.kbrom.charity_lens_backend.auth.dto.ChangePasswordDTO;
+import com.kbrom.charity_lens_backend.donorProfile.DonorProfile;
+import com.kbrom.charity_lens_backend.donorProfile.DonorProfileRepository;
+import com.kbrom.charity_lens_backend.user.dto.GetDonorProfileDTO;
+import com.kbrom.charity_lens_backend.user.dto.UpdateDonorProfileDTO;
 import com.kbrom.charity_lens_backend.common.exception.ResourceNotFoundException;
 import com.kbrom.charity_lens_backend.common.util.mappers.GetUserMapper;
-import com.kbrom.charity_lens_backend.common.util.mappers.RegisterUserMapper;
+import com.kbrom.charity_lens_backend.user.enums.Role;
 import com.kbrom.charity_lens_backend.user.model.User;
 import com.kbrom.charity_lens_backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
       private final UserRepository userRepository;
+      private final DonorProfileRepository donorRepository;
       private final GetUserMapper getUserMapper;
-      private final RegisterUserMapper registerUserMapper;
-      public GetUserDTO getUserByEmail(String email) {
-        User user= userRepository.findByEmail(email).orElseThrow(()->new ResourceNotFoundException("User Not Found"));
-        return getUserMapper.toDTO(user);
+      private final PasswordEncoder passwordEncoder;
+
+      public User getUserById(Long id) {
+          return userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User Not Found"));
       }
-      public List<GetUserDTO> getAllUsers(){
-          List<User> users= userRepository.findAll();
-          return getUserMapper.toDTO_List(users);
+      public User getUserByEmail(String email) {
+          return userRepository.findByEmail(email).orElseThrow(()->new ResourceNotFoundException("User Not Found"));
       }
-      public GetUserDTO getUserById(Long id) {
-          User user = userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User Not Found"));
-          return getUserMapper.toDTO(user);
+      public User getUserByUsername(String username) {
+          return userRepository.findByUsername(username).orElseThrow(()->new ResourceNotFoundException("User Not Found"));
       }
-      public void deleteUserById(Long id) {
-          User user = userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User Not Found"));
-          userRepository.delete(user);
+      public List<User> getAllUsers() {
+          return userRepository.findAll();
+      }
+      public void disableUserById(Long id) {
+          User user = getUserById(id);
+          if(user.isEnabled()) {
+              user.setEnabled(false);
+          }
+      }
+      public void enableUserById(Long id) {
+          User user = getUserById(id);
+          if(!user.isEnabled()) {
+              user.setEnabled(true);
+          }
+      }
+      public void flagUserById(Long id) {
+          User user = getUserById(id);
+          if(!user.isFlagged()) {
+              user.setFlagged(true);
+          }
+      }
+      public void unFlagUserById(Long id) {
+          User user = getUserById(id);
+          if(user.isFlagged()) {
+              user.setFlagged(false);
+          }
       }
 
-      public GetUserDTO updateUser(UserUpdateDTO updateUserDTO, Long id) {
-          User oldUser=userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User Not Found"));
-          if(updateUserDTO.getFirstName()!=null) {
-              oldUser.setFirstName(updateUserDTO.getFirstName());
+      public GetDonorProfileDTO updateDonorProfile(UpdateDonorProfileDTO updateDTO, Long id) {
+          User user=getUserById(id);
+          DonorProfile profile=user.getProfile();
+          if(updateDTO.getFirstName()!=null) {
+              profile.setFirstName(updateDTO.getFirstName());
           }
-          if(updateUserDTO.getLastName()!=null) {
-              oldUser.setLastName(updateUserDTO.getLastName());
+          if(updateDTO.getLastName()!=null) {
+              profile.setLastName(updateDTO.getLastName());
           }
-          if(updateUserDTO.getUsername()!=null) {
-              oldUser.setUsername(updateUserDTO.getUsername());
+          if(updateDTO.getPhoneNumber()!=null) {
+              profile.setPhoneNumber(updateDTO.getPhoneNumber()); // needs verification in production
           }
-          if(updateUserDTO.getPassword()!=null) {
-              oldUser.setPassword(updateUserDTO.getPassword());
-          }
-          return getUserMapper.toDTO(oldUser);
+          donorRepository.save(profile);
+
+          return getUserMapper.toDTO(profile);
       }
+      public void changePassword(Long id, ChangePasswordDTO changePasswordDTO, Principal principal) {
+          User user=getUserById(id);
+          boolean isAdmin=isAdmin(principal.getName());
+          String currentUsername=principal.getName();
+
+          if(!isAdmin&&!currentUsername.equals(user.getUsername())) {
+              throw new AccessDeniedException("You don't have permission to change other's password");
+          }
+         if(!isAdmin&&!passwordConfirmed(changePasswordDTO,user)){
+             throw new IllegalArgumentException("Incorrect password or new passwords does not match");
+         }
+         user.setPassword(passwordEncoder.encode(changePasswordDTO.getNewPassword()));
+         userRepository.save(user);
+
+      }
+      private boolean passwordConfirmed (ChangePasswordDTO changePasswordDTO,User user) {
+          return changePasswordDTO.getNewPassword().equals(changePasswordDTO.getConfirmNewPassword())
+                  && passwordEncoder.matches(user.getPassword(),changePasswordDTO.getOldPassword());
+      }
+      private boolean isAdmin (String username) {
+          User user=getUserByUsername(username);
+          return user.getRoles().contains(Role.SYS_ADMIN);
+
+      }
+
+
 
 }
