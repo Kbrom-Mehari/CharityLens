e4e0c5f12d80aed633c5e8eea98feeee80edf859
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-27 21:22:05 +0300

    moved to user package

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/user/donorProfile/DonorProfileRepository.java
@@ -0,0 +1,7 @@
+package com.kbrom.charity_lens_backend.user.donorProfile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DonorProfileRepository extends JpaRepository<DonorProfile, Long> {
+
+}
