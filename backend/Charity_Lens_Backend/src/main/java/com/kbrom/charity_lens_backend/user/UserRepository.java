84c7a522d41ae56644d3b686d22882b683d7268c
Kbrom Mehari <kibrommehari121@gmail.com>
2025-09-27 21:13:16 +0300

    moved one directory up

---
+++ backend/Charity_Lens_Backend/src/main/java/com/kbrom/charity_lens_backend/user/UserRepository.java
@@ -0,0 +1,15 @@
+package com.kbrom.charity_lens_backend.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmailAndPassword(String email, String password);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+}
